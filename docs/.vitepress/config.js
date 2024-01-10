17a638a4ac30aa8aa90191224f0c4c63f25ad30a
jiaHao <1366448731@qq.com>
2024-01-11 03:44:55 +0800

    修改base为post

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -2,8 +2,7 @@ const path = require('path');
 const fs = require('fs');
 export default {
   description: '2020_广州_程序员',
-  // base: '/post/',
-  base: '/',
+  base: '/post/',
   title:'findjh',
   titleTemplate: '找到我啦!',
   lastUpdated: true,
