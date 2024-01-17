e476a6077beda98749d4123d431b9d486ab227e1
jiaHao <1366448731@qq.com>
2024-01-17 16:41:51 +0800

    clip-path

--- docs/articles/React/next.js.md
+++ docs/articles/React/next.js.md
@@ -1,1 +1,4 @@
 [推荐一本学习nextjs的电子书](https://nextjs-in-action-cn.taonan.lu/)
+
+getInitialProps：第一次刷新的时候是服务端调用的，之后router或Link就是客户端调用的
+
---
+++ examples/ClipPath.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="container" ref="containerRef">
+        <img class="bottom" src="./assets/images/girl-14-2.jpg">
+        <div class="img-overlay" @mousemove="handleMouseMove">
+            <img src="./assets/images/girl-14.webp">
+        </div>
+        <div class="divider"></div>
+    </div>
+</template>
+<style scoped>
+.container {
+    position: relative;
+    width: 80%;
+    --exposure: 50%;
+
+}
+
+img {
+    width: 100%;
+}
+
+.img-overlay {
+    position: absolute;
+    inset: 0;
+}
+
+.img-overlay img {
+    width: 100%;
+    clip-path: inset(0 var(--exposure) 0 0);
+}
+
+.divider {
+    width: 1px;
+    height: 100%;
+    background-color: #666;
+    position: absolute;
+    top: 0;
+    right: var(--exposure)
+}
+</style>
+<script setup>
+import { ref } from 'vue';
+const containerRef = ref(null)
+function handleMouseMove(e) {
+    const exposure = containerRef.value.clientWidth - e.offsetX;
+    containerRef.value.style.setProperty('--exposure', `${exposure}px`)
+}
+</script>
